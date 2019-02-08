8f864fbee2efbf7503f664c37d9921500ad6289b
Maxence Bouret <maxence.bouret@gmail.com>
2019-02-08 12:31:53 +0100

    Update of the Home Page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
-const express = require('express');
-const router  = express.Router();
+const express = require('express')
+const User = require('../models/User')
+const Link = require('../models/Link')
+const Like = require('../models/Like')
+const { checkConnected } = require('../configs/middlewares')
+const router = express.Router()
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
-});
+  
+  Link.find()
+  .then(links => {
+      res.render('index', { 
+        links,
+        errorMessage: req.flash('errorMessage')[0]
+      })
+    })
+    .catch(next)
+})
 
-module.exports = router;
+router.post('/add-link', (req, res, next) => {
+  if (!req.user) {
+    req.flash('errorMessage', 'You need to be connected to add a link')
+    res.redirect('/')
+    return
+  }
+
+  Link.create({
+    url: req.body.url,
+    _owner: req.user._id
+  })
+  .then(() => {
+    res.redirect('/')
+  })
+  .catch(err => {
+    // Defines a variable `req.flash('errorMessage')[0]` for the next route
+    req.flash('errorMessage', 'Please, give a URL that starts with "http://" or "https://')
+    res.redirect('/')
+  })
+})
+
+module.exports = router
